1ddf09296f2e39487cce0823ca120d1f90b31395
lin <noemail@qq.com>
2020-04-11 17:26:33 +0800

    补充stream的一个案例

--- javaDemo/src/main/java/com/rei/javaDemo/example/StreamExample.java
+++ javaDemo/src/main/java/com/rei/javaDemo/example/StreamExample.java
@@ -1,13 +1,36 @@
 package com.rei.javaDemo.example;
 
+import com.rei.javaDemo.model.TestParam;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
  * java 8流处理示例
  */
 public class StreamExample {
+
+    private static List<TestParam> list = new ArrayList<>();
+
+    public StreamExample(){
+        TestParam p1 = new TestParam();
+        p1.setId(1);
+        p1.setName("test1");
+        p1.setSex("男");
+        TestParam p2 = new TestParam();
+        p1.setId(1);
+        p1.setName("test2");
+        p1.setSex("男");
+        TestParam p3 = new TestParam();
+        p1.setId(1);
+        p1.setName("test3");
+        p1.setSex("男");
+        list.add(p1);
+        list.add(p2);
+        list.add(p3);
+    }
 
     /**
      * 使用分页的示例，请注意分页的计算部分
@@ -45,5 +68,25 @@ public class StreamExample {
     public static List<String> page(List<String> sour, int pageSize, int pageNum) {
         List<String> res = sour.stream().skip(pageSize*(pageNum-1)).limit(pageSize).collect(Collectors.toList());
         return res;
+    }
+
+    /**
+     * 将一个集合中的某个字段取出来
+     * @return
+     */
+    public static String streamMap(){
+        // 这两种方式是等价的
+        List<String> list1 = list.stream().map(TestParam::getName).collect(Collectors.toList());
+        List<String> list2 = list.stream().map(e -> e.getName()).collect(Collectors.toList());
+        return "";
+    }
+
+    /**
+     * 集合分组
+     * @return
+     */
+    public static void streamMGroup(){
+        // 根据某个字段分组，然后可以方便根据分组key取出对应的数据
+        Map<String,List<TestParam>> map = list.stream().collect(Collectors.groupingBy(TestParam::getSex));
     }
 }
